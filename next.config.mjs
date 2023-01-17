d447974fd6b420e9838940985439d62175b1edea
kennedymeadows <62924402+kennedymeadows@users.noreply.github.com>
2023-01-17 11:21:14 -0800

    init commit

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  reactStrictMode: true,
+  images: {
+    loader: 'default',
+    domains: ['storage.googleapis.com', 'images.unsplash.com'],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
