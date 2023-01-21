e68b396f85087e28128872b9544a6437d966321c
kennedymeadows <lewissimonjohn@gmail.com>
2023-01-20 19:06:31 -0800

    apostrophe

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -61,7 +61,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I'm Simon Lewis. I live in Los Angeles where I study computer
+              I’m Simon Lewis. I live in Los Angeles where I study computer
               science.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
